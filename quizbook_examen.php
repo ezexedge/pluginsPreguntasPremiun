c801206dada212438e0d516d00f0048d57331219
ezequiel <ezeedge@gmail.com>
2020-11-08 19:16:55 -0300

    agregando el header

---
+++ quizbook_examen.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name:  Preguntas y respuestas examen premiun
+Plugin URI:
+Description:  Plugin para añadir examenes
+Version:      1.0
+Author:       Ezequiel Gallardo
+Author URI:
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  quizbook
+*/
+
+
