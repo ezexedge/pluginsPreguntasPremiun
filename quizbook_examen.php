a7d31dda3b2ab6525e7c2ad1190b3f1ffcbb40ef
ezequiel <ezeedge@gmail.com>
2020-11-08 19:31:13 -0300

    activar plugin antes de instalar el premiun

--- quizbook_examen.php
+++ quizbook_examen.php
@@ -12,3 +12,19 @@ Text Domain:  quizbook
 */
 
 
+function quizbook_examen_revisar() {
+    if(!function_exists( 'quizbook_post_type' ) ) {
+        deactivate_plugins(plugin_basename(__FILE__));
+        
+        add_action( 'admin_notices', 'quizbook_error_activar' );
+    } 
+}
+add_action('admin_init', 'quizbook_examen_revisar');
+
+function quizbook_error_activar() {
+    $class = 'notice notice-error';
+    $message = __( 'Un Error ocurrió! Necesitas Instalar el Plugin Quiz.', 'sample-text-domain' );
+
+    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) ); 
+}
+   
