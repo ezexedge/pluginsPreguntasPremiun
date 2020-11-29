be6aa41fa9aa198180d19fd6674d012635c6323f
ezequiel <ezeedge@gmail.com>
2020-11-28 21:02:25 -0300

    agregando role

--- quizbook_examen.php
+++ quizbook_examen.php
@@ -10,7 +10,8 @@ License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  quizbook
 */
-
+if ( ! defined( 'ABSPATH' ) ) exit;
+//reviso que el plugins este instalado
 
 function quizbook_examen_revisar() {
     if(!function_exists( 'quizbook_post_type' ) ) {
@@ -20,7 +21,7 @@ function quizbook_examen_revisar() {
     } 
 }
 add_action('admin_init', 'quizbook_examen_revisar');
-
+//mensaje de error
 function quizbook_error_activar() {
     $class = 'notice notice-error';
     $message = __( 'Un Error ocurrió! Necesitas Instalar el Plugin Quiz.', 'sample-text-domain' );
@@ -28,3 +29,13 @@ function quizbook_error_activar() {
     printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) ); 
 }
    
+
+/*
+* Registrar Post Types
+*/
+require_once plugin_dir_path(__FILE__) . 'includes/posttypes.php';
+register_activation_hook(__FILE__, 'quizbook_examenes_rewrite_flush'); 
+
+require_once plugin_dir_path( __FILE__ ) . 'includes/roles.php';
+register_activation_hook( __FILE__, 'quizbook_examenes_agregar_capabilities' );
+register_deactivation_hook( __FILE__, 'quizbook_examenes_remover_capabilities' );
