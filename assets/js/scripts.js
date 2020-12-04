452f1c15e5c1c102080f2b898a06bbe4802603d7
ezequiel <ezeedge@gmail.com>
2020-12-04 11:36:54 -0300

    ultimos cambios

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,3 @@
 (function($){
-console.log('test')
+    $('.chosen-select').chosen(); 
 })(jQuery)
--- includes/scripts.php
+++ includes/scripts.php
@@ -6,7 +6,7 @@ function quizbook_examen_styles($hook) {
 
     if ( $hook == 'post-new.php' || $hook == 'post.php' ) {
         if ( 'examenes' === $post->post_type ) {     
-            wp_enqueue_style('chosen', plugins_url('../assets/css/chosen.css', __FILE__));
+            wp_enqueue_style('chosen', plugins_url('../assets/css/chosen.min.css', __FILE__));
             wp_enqueue_script('chosenjs', plugins_url('../assets/js/chosen.jquery.min.js', __FILE__), array('jquery'), 1.0, true );
             
             wp_enqueue_script('quizbook_scripts', plugins_url('../assets/js/scripts.js', __FILE__), array(), 1.0, true );
